5c9c513c3b14807178f0a1a96fd605bde752125e
shihyuanwang <49223125+shihyuanwang@users.noreply.github.com>
2019-12-23 01:43:49 +0800

    Java

---
+++ Currency Exchange/CurrencyExchange.java
@@ -0,0 +1,101 @@
+// SHIH-YUAN WANG - Final Project
+/*
+Given a file with the USD to <CURR> conversion rate
+Convert X amount of Currency A to Currency B
+User inputs are the source and destination currency symbols and the amount.
+$3000 AUD = $42,641.40  MXN
+*/
+
+import java.io.FileReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class CurrencyExchange {
+    String sourceCurrency;
+    BigDecimal amount;
+    String destinationCurrency;
+
+    public CurrencyExchange(String sourceCurrency, BigDecimal amount, String destinationCurrency) {
+        this.sourceCurrency = sourceCurrency;
+        this.amount = amount;
+        this.destinationCurrency = destinationCurrency;
+    }
+
+    public BigDecimal ExchangeRate () {
+        Hashtable <String, BigDecimal> ht_rate = new Hashtable <String, BigDecimal> ();
+        try {
+            String contents = "";
+            Scanner in = new Scanner(new FileReader("src/forex.csv"));
+            int iteration = 0;
+            while (in.hasNextLine()) {
+                contents = in.nextLine();
+                //skip first column name line
+                if(iteration == 0) {
+                    iteration++;
+                    continue;
+                }
+                //use comma as separator
+                String[] curRate = contents.split(",");
+                //System.out.println(curRate[0]+ " " + curRate[2]);
+
+                //put currency and its rate into hashtable
+                ht_rate.put(curRate[0], new BigDecimal(curRate[2]));
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error " + e.getMessage());
+        }
+        //System.out.println(ht_rate);
+        //System.out.println(ht_rate.size());
+
+        //get source currency and destination currency rate from hashtable
+        BigDecimal rateS = ht_rate.get(this.sourceCurrency);
+        BigDecimal rateD = ht_rate.get(this.destinationCurrency);
+
+        //exchangeRate = destinationCurrency(rateD)/sourceCurrency(rateS)
+        BigDecimal exchangeRate = rateD.divide(rateS, 10, RoundingMode.HALF_UP);
+        return exchangeRate ;
+    }
+
+    public BigDecimal convertCurrency (BigDecimal exchangeRate) {
+        //convert currency using calculated exchangeRate
+        BigDecimal convertedAmount = this.amount.multiply(exchangeRate);
+        convertedAmount = convertedAmount.setScale(2, RoundingMode.HALF_UP);
+        return convertedAmount;
+    }
+
+    public static void main(String args[]) {
+        String again;
+        do { //prompts user for inputs
+            System.out.println("Inputs the source currency symbol: ");
+            Scanner scan = new Scanner(System.in);
+            String sourceCurrency = scan.nextLine().toUpperCase().trim();
+
+            System.out.println("Inputs the amount of the source currency: ");
+            Scanner scan2 = new Scanner(System.in);
+            BigDecimal amount = new BigDecimal(scan2.nextLine());
+
+            System.out.println("Inputs the destination currency symbol: ");
+            Scanner scan3 = new Scanner(System.in);
+            String destinationCurrency = scan3.nextLine().toUpperCase().trim();
+
+
+            //create new instance of CurrencyExchange class
+            CurrencyExchange ce = new CurrencyExchange(sourceCurrency, amount, destinationCurrency);
+            //calculate exchangeRate
+            BigDecimal exchangeRate = ce.ExchangeRate();
+            System.out.println("Exchange Rate: " + exchangeRate);
+            //convert currency
+            BigDecimal convertedAmount = ce.convertCurrency(exchangeRate);
+            System.out.println("$" + amount.toString() + " " + sourceCurrency + " = $" + convertedAmount.toString() + " " + destinationCurrency);
+            //ask users if they want to exchange again
+            System.out.println("\nDo you want to exchange again? (y/n)");
+            Scanner scan4 = new Scanner(System.in);
+            again = scan4.nextLine().toLowerCase().trim();
+            //if they want to exchange again, repeat the loop
+
+        } while (again.equals("y"));
+    }
+}
---
+++ Word Counter/WordCounter.java
@@ -0,0 +1,120 @@
+// SHIH-YUAN WANG - Word Counter
+
+import java.io.FileReader;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.LinkedHashMap;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class WordCounter {
+
+    public Hashtable count_words(String contents) {
+
+        Hashtable<String, Integer> ht_count = new Hashtable<String, Integer>();
+        String word;
+        //break a string into tokens
+        StringTokenizer st = new StringTokenizer(contents, " \t\n\r\f,.:;?!'—\"");
+        while (st.hasMoreTokens()) {
+            //convert all characters to lowercase
+            word = st.nextToken().toLowerCase();
+
+            //get word count times and insert into or update hashtable
+            if (ht_count.containsKey(word)) {
+                //get the value of the corresponding key
+                int count_times = (Integer) ht_count.get(word);
+                ht_count.put(word, count_times + 1);
+            } else {
+                ht_count.put(word, 1);
+            }
+        }
+        return ht_count;
+    }
+
+
+    public LinkedList top20(Hashtable ht_count) {
+        // TODO: Return top 20 words on the songs,
+        // cleaned up removing special characters ",' and in lowercase
+        // For example "A  should be converted to a
+        // (singular and plural are ok to have as duplicates)
+        // Return value can be an array or LinkedList
+        // Each element of the array can be a Hashtable with only one element sorted where the highest element is at the top
+        // For example:  [{the=20}, {a=10}, {boy=10}, {news=5}.....]
+
+        // 1. Convert Map to LinkedList of Map
+        LinkedList<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(ht_count.entrySet());
+
+        // 2. Sort list with Collections.sort() according to self-defined criteria(custom Comparator)
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> c1, Map.Entry<String, Integer> c2) {
+                //sort in descending order of value(count times), then in ascending order of key(word)
+                int cmp1 = c2.getValue().compareTo(c1.getValue());
+                if (cmp1 != 0) {
+                    //if they're not same values(count times), sort in descending order of value
+                    return cmp1;
+                } else {
+                    //if they're same values(count times), sort in ascending order of key(word)
+                    return (c1.getKey().compareTo(c2.getKey()));
+                }
+            }
+        });
+
+        // 3. Loop the sorted LinkedList and insert it into a new insertion order Map LinkedHashMap
+        Map <String, Integer> sortedCount = new LinkedHashMap <String, Integer>();
+        for (Map.Entry<String, Integer> entry : list) {
+            sortedCount.put(entry.getKey(), entry.getValue());
+        }
+
+        // 4. Obtain an Iterator for the entries set(key-value pairs) contained in the LinkedHashMap
+        Iterator <Map.Entry<String, Integer>> itr = sortedCount.entrySet().iterator();
+
+        // 5. Get first 20 items, insert into a Hashtable with only one element sorted, and return a LinkedList of Hashtable
+        LinkedList<Hashtable> countList = new LinkedList<Hashtable>();
+        // Iterate first 20 items through LinkedHashMap entries
+        int count = 0;
+        while (count < 20) {
+            //returns the next element
+            Map.Entry<String, Integer> countPair = itr.next();
+            Hashtable countTable = new Hashtable();
+            //insert the key and value of the element into Hashtable
+            countTable.put(countPair.getKey(), countPair.getValue());
+            //appends this Hashtable to the end of the LinkedList
+            countList.add(countTable);
+            count++;
+        }
+        return countList;
+    }
+
+
+    public static void main(String args[]){
+        try{
+            //Read the file
+            String contents = "";
+            Scanner in = new Scanner(new FileReader("src/Fear.txt"));
+            while(in.hasNextLine()) {
+                contents += in.nextLine() + "\n";
+            }
+            WordCounter wc = new WordCounter();
+
+            //count the times of each word
+            Hashtable count = wc.count_words(contents);
+            System.out.println(count);
+
+            //get top20 words
+            LinkedList top20 = wc.top20(count);
+            System.out.println("Top 20 words: " + top20);
+            int i = 1;
+            for (Object item: top20) {
+                System.out.println("Top " + i + " word: " + item);
+                i++;
+            }
+        } catch (Exception e){
+            System.err.println("Error " + e.getMessage());
+        }
+    }
+}
